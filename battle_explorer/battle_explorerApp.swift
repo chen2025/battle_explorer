94203cc34844b1f4f2aa02ed40da5995ce23b0ef
tchen <tchen@turnitin.com>
2024-10-30 13:07:25 -0400

    Initial Commit

---
+++ battle_explorer/battle_explorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  battle_explorerApp.swift
+//  battle_explorer
+//
+//  Created by Tony Chen on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct battle_explorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
